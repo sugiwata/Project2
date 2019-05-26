6fe73d331eaac8a90c3da72418e1480236cd4d30
syebun <k18030kk@aitech.ac.jp>
2019-05-27 01:12:13 +0900

    データベースとのやりとりの完成＋名前入力欄の削除

--- HTML5/public_html/app2.php
+++ HTML5/public_html/app2.php
@@ -1,9 +1,9 @@
 
 <?php
-$s=new PDO("mysql:host=localhost;dbname=","root","root");
-$ca_d=isset($_GET["category"])?htmlspecialchars($_GET["category"]):null;
-$na_d=isset($_GET["name"])?htmlspecialchars($_GET["name"]):null;
+$s=new PDO("mysql:host=localhost;dbname=pro2test","root","root");
+$ca_d=isset($_GET["category"])?$_GET["category"]:null;
 $te_d=isset($_GET["text"])?htmlspecialchars($_GET["text"]):null;
+$usid=isset($_SESSION["usid"])?htmlspecialchars($_SESSION["usid"]):null;
 
 print<<<eot1
 <!DOCTYPE html>
@@ -29,10 +29,55 @@ body{
 <body>
 eot1;
 
-if($ca_d==null&&$na_d==null&&$te_d==null){
-print <<<eot4
-<div id="title">投稿画面</div> 
-	<form method="GET" action="app2.php">
+if($usid==null){
+	print <<<eot5
+	<div>
+	ログインしていません
+	<br><a href='app3.php'>ここからログインしてください</a>
+	</div>
+	</body>
+	</HTML>
+eot5;
+}else{
+	if($ca_d==null&&$te_d==null){
+	print <<<eot4
+	<div id="title">投稿画面</div> 
+		<form method="GET" action="app2.php">
+				<label>カテゴリー
+					<select name="category">
+						<option value="">選択してください</option>
+						<option value="1">ゲーム</option>
+						<option value="2">スポーツ</option>
+						<option value="3">ミュージック</option>
+						<option value="4">ニュース</option>
+					</select>
+				</label>
+				<br>
+				<label>本文
+					<textarea id="text" name="text"></textarea>
+				</label>
+				<br>
+				<input type="submit" value="送信">
+			</form>
+			<br><a href="index.html">トップページに戻る</a>
+	</body>
+	</HTML>
+eot4;
+	}else if($ca_d!=""&&$te_d!=""){
+	$s->query("INSERT INTO toko_master VALUES (0,'$ca_d','$usid',now(),'$te_d',0)");
+	print <<<eot2
+	投稿しました
+	<br><a href="index.html">トップページに戻る</a>
+	<a href="app1.html">カテゴリ一覧へ</a>
+	<a href="app4.php">閲覧画面へ</a>
+	</body>
+	</html>
+eot2;
+	}else{
+	print <<<eot3
+	<div id="title">投稿画面</div> 
+	<div style="font-color:red,font-size:30px">入力されていない項目があります</div>
+		<form method="GET" action="app2.php">
 			<label>カテゴリー
 				<select name="category">
 					<option value="">選択してください</option>
@@ -43,60 +88,16 @@ print <<<eot4
 				</select>
 			</label>
 			<br>
-			<label>名前
-				<input type="text" name="name">
-			</label>
-			<br>
-			<label>本文
-				<textarea id="text" name="text"></textarea>
-			</label>
-			<br>
-			<input type="submit" value="送信">
-      </form>
-      <br><a href="index.html">トップページに戻る</a>
-  </body>
-</HTML>
-eot4;
-}else if($ca_d!=""&&$na_d!=""&&$te_d!=""){
-$s->query("INSERT INTO $category (name,mess) VALUES ('$name','$text')");
-print <<<eot2
-投稿しました
-<br><a href="index.html">トップページに戻る</a>
-<a href="app1.html">カテゴリ一覧へ</a>
-<a href="app4.php">閲覧画面へ</a>
-</body>
-</html>
-eot2;
-}else{
-print <<<eot3
-<div id="title">投稿画面</div> 
-<div style="font-color:red,font-size:30px">入力されていない項目があります</div>
-	<form method="GET" action="app2.php">
-			<label>カテゴリー
-				<select name="category">
-					<option value="">選択してください</option>
-					<option value="1">スポーツ</option>
-					<option value="2">ゲーム</option>
-					<option value="3">ミュージック</option>
-					<option value="4">ニュース</option>
-				</select>
-			</label>
-			<br>
-			<label>名前
-				<input type="text" name="name" value="$na_d">
-			</label>
-			<br>
 			<label>本文
 				<textarea id="text" name="text">$te_d</textarea>
 			</label>
 			<br>
 			<input type="submit" value="送信">
-      </form>
-      <br><a href="index.html">トップページに戻る</a>
-</body>
-</html>
+		</form>
+	<br><a href="index.html">トップページに戻る</a>
+	</body>
+	</html>
 eot3;
+	}
 }
 ?>
-</body>
-</HTML>
