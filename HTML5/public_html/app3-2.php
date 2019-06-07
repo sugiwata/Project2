bfc32c48550c1c3997563f955c76f1cdb0cda138
sugiwata <k18053kk@aitech.ac.jp>
2019-06-07 12:48:55 +0900

    ファイル名変更&共通イメージ適用

--- HTML5/public_html/app3-2.php
+++ HTML5/public_html/app3-2.php
@@ -10,74 +10,76 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style type="text/css">
-            * {
-                text-align: center;
-            }
-            div {
-                font-size: 2em;
-            }
-            button {
-                font-size: large;
-                width: 40%;
-                height: 4em;
-            }
-            body{
+        * {
+              text-align: center;
+          }
+          text {
+              font-size: 28px;
+              Color: red;
+          }
+          text2 {
+              font-size: 23px;
+          }
+          div {
+              font-size: 4em;
+              font-weight: bold;
+          }
+         .sub {
+              display       : inline-block;
+              border-radius : 5%;          /* 角丸       */
+              font-size     : 15pt;        /* 文字サイズ */
+              text-align    : center;      /* 文字位置   */
+              cursor        : pointer;     /* カーソル   */
+              padding       : 6px 6px;   /* 余白       */
+              background    : #ffffff;     /* 背景色     */
+              color         : #000000;     /* 文字色     */
+              line-height   : 1em;         /* 1行の高さ  */
+              transition    : .3s;         /* なめらか変化 */
+              box-shadow    : 1px 1px 3px #666666;  /* 影の設定 */
+              border        : 2px solid #ffffff;    /* 枠の指定 */
+          }
+          .sub:hover {
+              box-shadow    : none;        /* カーソル時の影消去 */
+              color         : #ffffff;     /* 背景色     */
+              background    : #000000;     /* 文字色     */
+          }
+          button {
+              font-size: large;
+              width: 40%;
+              height: 4em;
+
+          }
+          body{
               background-color: rgb(0, 250, 213);
-            }
+          }
         </style>
     </head>
     <body>
         <div>ユーザ登録</div>
-        <?php
+    <?php
+      $sUserID=$_POST["fUserID"];
+      $sUserName=$_POST['fUserName'];
+      $sPassWd1=$_POST['fPassWd1'];
+      $sPassWd2=$_POST['fPassWd2'];
 
-        //以下のプログラムはhttp://www.manabukun.net/kyouzai/php/php16.htmから引用
-        $sUserID=$_POST["fUserID"];
-        $sUserName=$_POST['fUserName'];
-        $sPassWd1=$_POST['fPassWd1'];
-        $sPassWd2=$_POST['fPassWd2'];
+      $s=new PDO("mysql:host=localhost;dbname=pro2test","root","root");
 
-        //以下のプログラムは、確認用のパスワードと一致しているかをチェックしている。
-        //不一致のときは、そのままプログラムを終了している。（exit）
+      if($sPassWd1 != $sPassWd2){
+          echo "<FONT COLOR=\"RED\">パスワードが一致しません。もう一度登録しなおしてください。
+               </FONT><br>";
 
-        if($sPassWd1!=$sPassWd2){
-          echo "<FONT COLOR=\"RED\">パスワードが一致しません。もう一度登録しなおしてください。</FONT><br>";
-          exit();
-        }
-        else{
-          /*$conn=mysqli_connect('localhost','root','root') or exit("MySQLへ接続できません。");
-          mysqli_select_db($conn,'pro2test') or  exit("データベース名が間違っています。");*/
-
-          $s=new PDO("mysql:host=localhost;dbname=pro2test","root","root");
-          print "成功しました";
-
-        //以下のSQL文は、同じユーザ名が存在するかを調べるためのものである。
-        //パスワードをチェックしないのは、もし万が一、一致してしまうとユーザ名とパスワードがばれてしまうからである。
-        /*$sql="SELECT * FROM user_master where userName='{$sUserName}';";
-        $result=mysqli_query($conn,$sql) or exit("データの抽出に失敗しました。");
-        */
-        //$re=$s->query("SELECT count(*) FROM user_master where username='{$sUserName}'");
+      }else{
+        $s->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+        $re = $s->query("SELECT count(*) FROM user_master WHERE username ='$sUserName'");
+        $count = $re->fetchColumn();
         //データ件数が０件の場合
-      /*  if($re != 0){
-          echo "すでに{$sUserName}の名前は登録済みです。もう一度別の名前で登録してください。</FONT><br>";
-          exit()
-
-
-      /*  //以下のプログラムは、すでに同じユーザ名が存在すれば、登録済みのメッセージを出すためのものである。
-        if(mysqli_num_rows($result)!=0){
-          echo "すでに{$sUserName}の名前は登録済みです。もう一度別の名前で登録してください。<br>";
-        }
-      */
+        if($count !=  0){
+           echo "すでに{$sUserName}の名前は登録済みです。もう一度別の名前で登録してください。
+                </FONT><br>";
         }else{
-          //以下のプログラムは新規登録を行うためのプログラムである。
-          /*
-          $sql="INSERT INTO user_master values('{$sUserID}','{$sPassWd1}','{$sUserName}');";
-          $result=mysqli_query($conn,$sql) or exit("データの書き込みに失敗しました。");
-          echo "{$sUserName}のID登録が完了しました。";
-          */
-          //$s->query("INSERT INTO user_master VALUES ('{$sUserID}','{$sPassWd1}','{$sUserName}')");
-          echo "{$sUserName}のID登録が完了しました。";
+           $s->query("INSERT INTO user_master VALUES ('$sUserID','$sPassWd1','$sUserName')");
+           echo "{$sUserName}のID登録が完了しました。";
         }
-
       }
     ?>
     <br>
